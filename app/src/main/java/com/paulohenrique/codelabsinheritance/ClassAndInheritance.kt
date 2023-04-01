0237ba3fcbbd3fb98482b0f866b6bfc5d805a546
Paulo Henrique <34384560+paulohssouza@users.noreply.github.com>
2023-04-01 11:03:59 -0300

    1 - Implementation of function to calculate area

--- app/src/main/java/com/paulohenrique/codelabsinheritance/ClassAndInheritance.kt
+++ app/src/main/java/com/paulohenrique/codelabsinheritance/ClassAndInheritance.kt
@@ -1,4 +1,5 @@
 package com.paulohenrique.codelabsinheritance
+import kotlin.math.PI
 
 fun main() {
     fun printProprieties(dwelling: Dwelling) {
@@ -7,13 +8,14 @@ fun main() {
             println("Capacity: ${capacity}")
             println("Material: ${buildingMaterial}")
             println("Has room? ${hasRoom()}")
+            println("Floor area: %.2f".format(floorArea()))
         }
     }
-    val squareCabin = SquareCabin(6)
+    val squareCabin = SquareCabin(6, 50.0)
     printProprieties(squareCabin)
-    val roundHut = RoundHut(3)
+    val roundHut = RoundHut(3, 10.0)
     printProprieties(roundHut)
-    val roundTower = RoundTower(4)
+    val roundTower = RoundTower(4, 15.5)
     printProprieties(roundTower)
 }
 
@@ -25,23 +27,37 @@ abstract class Dwelling(private var residents: Int) {
     fun hasRoom(): Boolean {
         return residents < capacity
     }
+    abstract fun floorArea(): Double
 }
 
-class SquareCabin(residents: Int) : Dwelling(residents) {
+class SquareCabin(residents: Int, val length: Double) : Dwelling(residents) {
     override val nameConstruction = "Square Cabin"
     override val buildingMaterial = "Wood"
     override val capacity = 6
+
+    override fun floorArea(): Double {
+        return length * length
+    }
 }
 
-open class RoundHut(residents: Int) : Dwelling(residents) {
+open class RoundHut(residents: Int, val radius: Double) : Dwelling(residents) {
     override val nameConstruction = "Round Hut"
     override val buildingMaterial = "Straw"
     override val capacity = 4
+
+    override fun floorArea(): Double {
+        return PI * radius * radius
+    }
 }
 
 class RoundTower(residents: Int,
-                 val floors: Int = 2) : RoundHut(residents) {
+                 radius: Double,
+                 val floors: Int = 2) : RoundHut(residents, radius) {
     override val nameConstruction = "Round Tower"
     override val buildingMaterial = "Stone"
     override val capacity = 4 * floors
+
+    override fun floorArea(): Double {
+        return super.floorArea() * floors
+    }
 }
